1c7e1f4f27a39d339f11c9f39c83f058654c05cd
brendanee <brendanee314@gmail.com>
2025-07-06 02:27:05 -0700

    Style changes, working add/delete for list

--- list.js
+++ list.js
@@ -1,5 +1,6 @@
 import { read, readAll, write, del } from "./firebase.js";
 
+let list = [];
 
 async function init() {
   const today = new Date;
@@ -12,7 +13,7 @@ async function init() {
     (new Date(today.getFullYear(), today.getMonth() - 1, 0)).getDate() - today.getDate() // End of the current month minus today (scuffed)
   ]
   document.querySelectorAll('#list-input-wrapper > button').forEach((e, i) => e.addEventListener('click', () => addListItem(btnDayTimes[i]), false));
-  let list = await readAll('list');
+  list = await readAll('list');
   console.log(list);
   drawList(list);
 }
@@ -22,31 +23,34 @@ async function init() {
  * @param {Array} list List of items to draw to the screen
  */
 function drawList(list) {
+  document.getElementById('list-content').innerHTML = '';
   list.forEach((e) => {
     const ele = document.createElement('li');
     // Due date minus now (nanoseconds) to ms to sec to min to hr
-    const daysUntil = Math.floor((e.due - Date.now()) / 1000 / 60 / 60 / 24);
+    const daysUntil = Math.floor((e.due - Date.now()) / 1000 / 60 / 60 / 24) + 1;
     if (e.completed) {
       ele.innerHTML = `${e.value}<span class="list-delete">🗑️</span>`;
-      ele.className = 'list-completed';
+      ele.className = 'completed';
     } else {
       ele.innerHTML = `${e.value}<span class="list-done">✅</span><span class="list-important">📌</span><span class="list-delete">🗑️</span>`;
       if (daysUntil < 1) {
         ele.className = 'due';
       } else {
-        ele.className = e.important ? 'important' : '' + e.completed ? 'important' : '';
+        ele.className = (e.important ? 'important' : '') + (e.completed ? 'completed' : '');
       }
       
     }
     
     document.getElementById('list-content').append(ele);
+    document.querySelector('#list-content > li:last-of-type > .list-delete').addEventListener('click', () => deleteListItem(e.creation), false);
   });
 }
 
 document.addEventListener('firebasedone', init, false);
 
 async function addListItem(dueDate) {
-  const input = document.getElementById('list-input')
+  const input = document.getElementById('list-input');
+  if (!input.value) {return;}
   const today = Date.now();
   const hashtagSplit = input.value.split(' #')
   const value = hashtagSplit.shift();
@@ -59,6 +63,14 @@ async function addListItem(dueDate) {
     value: value,
   }
   write('list', "" + today, item);
-  
   input.value = '';
+  list.push(item);
+  drawList(list);
+}
+
+async function deleteListItem(creation) {
+  await del('list', "" + creation);
+  debugger
+  list = list.filter((e) => (e.creation !== creation));
+  drawList(list);
 }
